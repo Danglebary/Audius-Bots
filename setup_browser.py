74be96ab7e1d37f0b26713d9b7f2828ae658411b
Danglebary <halfblowncontact@gmail.com>
2021-09-15 06:47:41 -0700

    added type annotations

--- setup_browser.py
+++ setup_browser.py
@@ -1,18 +1,25 @@
 # IMPORT SELENIUM DEPENDENCIES
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 # CONFIGURE HEADLESS BROWSER OPTIONS
 opt_headless = Options()
-opt_headless.add_argument('--headless')
+opt_headless.add_argument("--headless")
 
 # CREATE HEADLESS BROWSER INSTANCE FOR BOTS
-def init_browser_headless():
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=opt_headless)
+def init_browser_headless() -> WebDriver:
+    browser: WebDriver = webdriver.Chrome(
+        executable_path=ChromeDriverManager().install(),
+        chrome_options=opt_headless,
+    )
     return browser
 
+
 # CREATE HEADFUL BROWSER INSTANCE FOR BOTS
-def init_browser_headful():
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+def init_browser_headful() -> WebDriver:
+    browser: WebDriver = webdriver.Chrome(
+        executable_path=ChromeDriverManager().install()
+    )
     return browser
