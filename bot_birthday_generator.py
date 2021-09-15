7360f3cd529393e6336d3ebed07cf8f9e20f558e
Danglebary <halfblowncontact@gmail.com>
2021-09-15 06:55:02 -0700

    added type annotations

--- bot_birthday_generator.py
+++ bot_birthday_generator.py
@@ -1,13 +1,15 @@
-# INIT GENERAL IMPORTS
+# GENERAL IMPORTS
 from datetime import datetime, timedelta
 from random import randint
 
 
 # CHOOSE RANDOM AGE AND BIRTH DATE BETWEEN MIN AND MAX AGE RANGE,
 # RETURN BIRTH DATE
-def gen_birthdate(max_age):
-    min_age = 18
-    date_format = "%m-%d-%Y"
-    birth_time = datetime.today() - timedelta(days=randint(365 * min_age, 365 * max_age))
-    birth_date = birth_time.strftime(date_format)
+def gen_birthdate(max_age: int) -> str:
+    min_age: int = 18
+    date_format: str = "%m-%d-%Y"
+    birth_time: datetime = datetime.today() - timedelta(
+        days=randint(365 * min_age, 365 * max_age)
+    )
+    birth_date: str = birth_time.strftime(date_format)
     return birth_date
