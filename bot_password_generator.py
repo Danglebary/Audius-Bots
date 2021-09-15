f97ce65d41389a381f03a11c9144b4544e617a12
Danglebary <halfblowncontact@gmail.com>
2021-09-15 06:53:34 -0700

    added type annotations

--- bot_password_generator.py
+++ bot_password_generator.py
@@ -1,11 +1,18 @@
-# INIT GENERAL IMPORTS
+# GENERAL IMPORTS
 import random, string
 
 # FUNCTION TO GENERATE RANDOM PASSWORD FOR BOT WITH 16 RANDOM CHARACTERS
-def gen_bot_pass():
-    pass_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-    password = []
+def gen_bot_pass() -> str:
+    pass_chars: str = "".join(
+        [
+            string.ascii_lowercase,
+            string.ascii_uppercase,
+            string.digits,
+            string.punctuation,
+        ]
+    )
+    password: list[str] = []
     for _ in range(16):
         password.append(random.choice(pass_chars))
-    full_pass = ''.join(password)
+    full_pass = "".join(password)
     return full_pass
