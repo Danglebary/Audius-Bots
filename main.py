c069eb4b09ec6dcb62a2da9783e9322539fde9a9
Danglebary <halfblowncontact@gmail.com>
2022-01-23 18:05:01 -0600

    load more bots

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from bot_routines import bot_routine_one
 if __name__ == "__main__":
     import multiprocessing as mp
 
-    bot_list: list[BotData] = query_first_x_bots(1)
+    bot_list: list[BotData] = query_first_x_bots(4)
     pool: Pool = mp.Pool(len(bot_list))
     results = pool.starmap(bot_routine_one, bot_list)
     print(results)
